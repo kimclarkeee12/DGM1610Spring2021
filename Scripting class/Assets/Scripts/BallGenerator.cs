ed3451f343fcb729b4da29257c4143fdfb34693d
Kim Clarke <kim.clarkeee12@gmail.com>
2021-02-26 13:58:12 -0700

    Adding scripts

---
+++ Scripting class/Assets/Scripts/BallGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+    private int number = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IEnumerator Start()
+        {
+            while (number < 100)
+            {
+                yield return new WaitForSeconds(0.5f);
+                Instantiate(ball);
+            }
+        }
+    }
+}
